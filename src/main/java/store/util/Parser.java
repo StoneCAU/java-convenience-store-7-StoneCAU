ffbcb49025bce6edb44da2586c390dbc6fe9816b
StoneCAU <seokwoo1999@naver.com>
2024-11-09 03:38:46 +0900

    feat(Parser): 주문 정보에 대한 사용자 입력 값 파싱
    - 정규표현식을 통한 검증 후 파싱

---
+++ src/main/java/store/util/Parser.java
@@ -0,0 +1,27 @@
+package store.util;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import store.exception.ErrorMessage;
+import store.exception.StoreException;
+
+public class Parser {
+    private final static String DELIMITER = ",";
+
+    public static List<String> parseItems(String input) {
+        validate(input);
+
+        return Stream.of(input.replaceAll("[\\[\\]]", "").split(DELIMITER))
+                .filter(item -> item.matches("[^\\-]+-\\d+"))
+                .collect(Collectors.toList());
+    }
+
+    private static void validate(String input) {
+        if (isNotMatch(input)) throw new StoreException(ErrorMessage.INVALID_FORMAT.getMessage());
+    }
+
+    private static boolean isNotMatch(String input) {
+        return !input.matches("[^\\-]+-\\d+,([^\\-]+-\\d+)*");
+    }
+}
