a0d2f5664bbbf5b6962faa24b7f946670d709169
StoneCAU <seokwoo1999@naver.com>
2024-11-09 23:52:20 +0900

    refactor(Application): Controller 생성 의존성 추가
    - PromotionFactory

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -2,10 +2,18 @@ package store;
 
 import store.controller.StoreController;
 import store.domain.product.ProductFactory;
+import store.domain.promotion.PromotionFactory;
+import store.service.InventoryService;
+import store.service.OrderService;
 
 public class Application {
     public static void main(String[] args) {
-        StoreController storeController = new StoreController(new ProductFactory());
+        final ProductFactory productFactory = new ProductFactory();
+        final InventoryService inventoryService = new InventoryService(productFactory);
+        final OrderService orderService = new OrderService();
+        final PromotionFactory promotionFactory = new PromotionFactory();
+
+        StoreController storeController = new StoreController(inventoryService, orderService, promotionFactory);
         storeController.run();
     }
 }
