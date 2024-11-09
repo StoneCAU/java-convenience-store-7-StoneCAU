84fb93943d1346fe6b4cf9ba67f2a4617a4dec52
StoneCAU <seokwoo1999@naver.com>
2024-11-09 20:31:00 +0900

    refactor(StoreController): 클래스 변경으로 인해  run 흐름 수정

--- src/main/java/store/controller/StoreController.java
+++ src/main/java/store/controller/StoreController.java
@@ -1,29 +1,55 @@
 package store.controller;
 
+import java.util.List;
+import java.util.function.Supplier;
+import store.domain.inventory.InventoryManager;
 import store.domain.product.ProductFactory;
-import store.domain.product.Products;
+import store.domain.inventory.Inventory;
+import store.dto.Order;
+import store.exception.StoreException;
+import store.service.InventoryService;
+import store.util.Parser;
 import store.view.InputView;
 import store.view.OutputView;
 
 public class StoreController {
-    private final ProductFactory productFactory;
+    private final InventoryService inventoryService;
 
-    public StoreController(ProductFactory productFactory) {
-        this.productFactory = productFactory;
+    public StoreController(InventoryService inventoryService) {
+        this.inventoryService = inventoryService;
     }
 
     public void run() {
         OutputView.printWelcomeMessage();
 
-        Products products = productFactory.createProducts();
-        loadInventory(products);
+        Inventory inventory = inventoryService.getInventory();
+        loadInventory(inventory);
 
-        InputView.purchaseProducts();
+        InventoryManager inventoryManager = new InventoryManager(inventory);
+        Order order = getOrder(inventoryManager);
     }
 
-    private void loadInventory(Products products) {
-        OutputView.printInventory(products);
+    private void loadInventory(Inventory inventory) {
+        OutputView.printInventory(inventory);
     }
 
+    private Order getOrder(InventoryManager inventoryManager) {
+        List<String> items = retryOnError(() -> {
+            String input = InputView.purchaseProducts();
+            return Parser.parseItems(input);
+        });
+
+        return inventoryManager.purchase(items);
+    }
+
+    private <T> T retryOnError(Supplier<T> supplier) {
+        while(true) {
+            try {
+                return supplier.get();
+            } catch (StoreException e) {
+                OutputView.printErrorMessage(e.getMessage());
+            }
+        }
+    }
 
 }
