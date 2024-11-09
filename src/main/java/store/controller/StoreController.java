8c1966409f37fe7266511f7a1ef3e0a420078178
StoneCAU <seokwoo1999@naver.com>
2024-11-09 23:51:44 +0900

    refactor(StoreController): 메서드 로직 변경
    - run: promotionFactory를 통해 프로모션 정보를 받아옴
    - getOrder: Inventory를 매개로하여 입력받은 값 검증

--- src/main/java/store/controller/StoreController.java
+++ src/main/java/store/controller/StoreController.java
@@ -2,44 +2,48 @@ package store.controller;
 
 import java.util.List;
 import java.util.function.Supplier;
-import store.domain.inventory.InventoryManager;
-import store.domain.product.ProductFactory;
 import store.domain.inventory.Inventory;
+import store.domain.promotion.Promotion;
+import store.domain.promotion.PromotionFactory;
 import store.dto.Order;
 import store.exception.StoreException;
 import store.service.InventoryService;
+import store.service.OrderService;
 import store.util.Parser;
 import store.view.InputView;
 import store.view.OutputView;
 
 public class StoreController {
     private final InventoryService inventoryService;
+    private final OrderService orderService;
+    private final PromotionFactory promotionFactory;
 
-    public StoreController(InventoryService inventoryService) {
+    public StoreController(InventoryService inventoryService, OrderService orderService, PromotionFactory promotionFactory) {
         this.inventoryService = inventoryService;
+        this.orderService = orderService;
+        this.promotionFactory = promotionFactory;
     }
 
     public void run() {
         OutputView.printWelcomeMessage();
 
         Inventory inventory = inventoryService.getInventory();
+        List<Promotion> promotions = promotionFactory.initPromotions();
         loadInventory(inventory);
 
-        InventoryManager inventoryManager = new InventoryManager(inventory);
-        Order order = getOrder(inventoryManager);
+        Order order = getOrder(inventory);
     }
 
     private void loadInventory(Inventory inventory) {
         OutputView.printInventory(inventory);
     }
 
-    private Order getOrder(InventoryManager inventoryManager) {
-        List<String> items = retryOnError(() -> {
+    private Order getOrder(Inventory inventory) {
+        return retryOnError(() -> {
             String input = InputView.purchaseProducts();
-            return Parser.parseItems(input);
+            List<String> items = Parser.parseItems(input);
+            return orderService.create(items, inventory);
         });
-
-        return inventoryManager.purchase(items);
     }
 
     private <T> T retryOnError(Supplier<T> supplier) {
