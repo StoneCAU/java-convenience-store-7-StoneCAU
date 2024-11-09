5979eb9a8512a6a34a839a038f51c828d7e9e08c
StoneCAU <seokwoo1999@naver.com>
2024-11-10 01:38:35 +0900

    refactor(ProductFactory): Product 생성 메서드에 Promotion 추가

--- src/main/java/store/domain/product/ProductFactory.java
+++ src/main/java/store/domain/product/ProductFactory.java
@@ -2,28 +2,29 @@ package store.domain.product;
 
 import java.util.Arrays;
 import java.util.List;
+import store.domain.promotion.Promotion;
 import store.util.FileLoader;
 
 public class ProductFactory {
     private static final String DELIMITER = ",";
     private static final String FILE_NAME = "products.md";
 
-    public List<Product> initProducts() {
+    public List<Product> initProducts(List<Promotion> promotions) {
         return FileLoader.loadMarkdownFile(FILE_NAME)
                 .stream()
                 .skip(1)
-                .map(this::getFields)
-                .map(this::create)
+                .map(item -> create(getFields(item), promotions))
                 .toList();
     }
 
-    private Product create(List<String> fields) {
+    private Product create(List<String> fields, List<Promotion> promotions) {
         String name = fields.getFirst();
         int price = Integer.parseInt(fields.get(1));
         int quantity = Integer.parseInt(fields.get(2));
-        String promotion = parsePromotion(fields.get(3));
+        String promotionName = parsePromotion(fields.get(3));
+        Promotion promotion = findPromotionByName(promotions, promotionName);
 
-        return new Product(name, price, quantity, promotion);
+        return new Product(name, price, quantity, promotionName, promotion);
     }
 
     private List<String> getFields(String item) {
@@ -37,6 +38,13 @@ public class ProductFactory {
         }
 
         return promotion;
+    }
+
+    private Promotion findPromotionByName(List<Promotion> promotions, String name) {
+        return promotions.stream()
+                .filter(promotion -> promotion.getName().equals(name))
+                .findFirst()
+                .orElse(null);
     }
 }
 
