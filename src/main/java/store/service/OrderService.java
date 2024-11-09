e99d3d0bf675a9cfe9bd258d0a4945e719d162ec
StoneCAU <seokwoo1999@naver.com>
2024-11-09 23:49:41 +0900

    feat(OrderService): 상품 이름 및 재고 수량 검증
    - OrderLine 레코드 생성 시에 검증

--- src/main/java/store/service/OrderService.java
+++ src/main/java/store/service/OrderService.java
@@ -1,28 +1,46 @@
 package store.service;
 
 import java.util.List;
+import store.domain.inventory.Inventory;
 import store.dto.Order;
 import store.dto.OrderLine;
+import store.exception.ErrorMessage;
+import store.exception.StoreException;
 
 public class OrderService {
     private static final String DELIMITER = "-";
 
-    public Order create(List<String> items) {
-        return makeAnOrder(items);
+    public Order create(List<String> items, Inventory inventory) {
+        return makeAnOrder(items, inventory);
     }
 
-    private Order makeAnOrder(List<String> items) {
+    private Order makeAnOrder(List<String> items, Inventory inventory) {
         return new Order(items.stream()
-                .map(this::makeAnOrderLine)
+                .map(item -> makeAnOrderLine(item, inventory))
                 .toList());
     }
 
-    private OrderLine makeAnOrderLine(String item) {
+    private OrderLine makeAnOrderLine(String item, Inventory inventory) {
         String[] split = item.split(DELIMITER);
 
         String name = split[0];
+        validateName(name, inventory);
+
         int quantity = Integer.parseInt(split[1]);
+        validateQuantity(name, quantity, inventory);
 
         return new OrderLine(name, quantity);
+    }
+
+    private void validateName(String name, Inventory inventory) {
+        if (inventory.isInvalidItem(name)) {
+            throw new StoreException(ErrorMessage.INVALID_ITEM.getMessage());
+        }
+    }
+
+    private void validateQuantity(String name, int quantity, Inventory inventory) {
+        if (inventory.isExceedQuantity(name, quantity)) {
+            throw new StoreException(ErrorMessage.EXCEED_QUANTITY.getMessage());
+        }
     }
 }
