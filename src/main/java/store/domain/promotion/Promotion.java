dab1ab9ea58cf8a75790ecc3c8be00af75da78db
StoneCAU <seokwoo1999@naver.com>
2024-11-10 01:39:07 +0900

    feat(Promotion): 각 필드에 대한 getter 함수 구현

--- src/main/java/store/domain/promotion/Promotion.java
+++ src/main/java/store/domain/promotion/Promotion.java
@@ -1,11 +1,11 @@
 package store.domain.promotion;
 
 public class Promotion {
-    private final String name;
-    private final int buy;
-    private final int get;
-    private final String start_date;
-    private final String end_date;
+    private String name;
+    private int buy;
+    private int get;
+    private String start_date;
+    private String end_date;
 
     public Promotion(String name, int buy, int get, String start_date, String end_date) {
         this.name = name;
@@ -13,5 +13,25 @@ public class Promotion {
         this.get = get;
         this.start_date = start_date;
         this.end_date = end_date;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getBuy() {
+        return buy;
+    }
+
+    public int getGet() {
+        return get;
+    }
+
+    public String getStart_date() {
+        return start_date;
+    }
+
+    public String getEnd_date() {
+        return end_date;
     }
 }
