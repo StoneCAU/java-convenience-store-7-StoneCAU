900ddd90918072cb32b6cef468f0c910688b804a
StoneCAU <seokwoo1999@naver.com>
2024-11-11 07:41:55 +0900

    feat(InputView): 추가 구매 여부 input 메서드 추가
    - inputAdditionalPurchase

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -9,6 +9,7 @@ public class InputView {
     private final static String GET_FREE_MESSAGE = "현재 %s은(는) 1개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)";
     private final static String NOT_DISCOUNT_MESSAGE = "현재 %s %d개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)";
     private final static String MEMBERSHIP_DISCOUNT_MESSAGE = "멤버십 할인을 받으시겠습니까? (Y/N)";
+    private final static String ADDITIONAL_PURCHASE_MESSAGE = "감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)";
 
     public static String purchaseProducts() {
         System.out.println(PURCHASE_MESSAGE);
@@ -30,6 +31,12 @@ public class InputView {
     public static String inputMembershipDiscount() {
         System.out.print(NEW_LINE);
         System.out.println(MEMBERSHIP_DISCOUNT_MESSAGE);
+        return input();
+    }
+
+    public static String inputAdditionalPurchase() {
+        System.out.printf(NEW_LINE);
+        System.out.println(ADDITIONAL_PURCHASE_MESSAGE);
         return input();
     }
 
