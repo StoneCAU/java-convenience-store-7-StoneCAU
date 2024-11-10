b4e25200469ffd2d9d57ed6817f2f583866d1ecb
StoneCAU <seokwoo1999@naver.com>
2024-11-10 15:35:32 +0900

    feat(InputView): 프로모션 적용 여부 input 메서드
    - getFreeProduct

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -1,12 +1,19 @@
 package store.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import store.domain.product.Product;
 
 public class InputView {
     private final static String PURCHASE_MESSAGE = "구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])";
+    private final static String GET_FREE_MESSAGE = "현재 %s은(는) 1개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)";
 
     public static String purchaseProducts() {
         System.out.println(PURCHASE_MESSAGE);
+        return input();
+    }
+
+    public static String getFreeProduct(Product product) {
+        System.out.printf((GET_FREE_MESSAGE) + "%n", product.getName());
         return input();
     }
 
