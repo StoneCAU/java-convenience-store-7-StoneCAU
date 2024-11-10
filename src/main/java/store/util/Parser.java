f70a53f5e998e73081d2168120b5626f0c25a711
StoneCAU <seokwoo1999@naver.com>
2024-11-10 15:36:17 +0900

    feat(Parser): Y/N을 통해 boolean을 return하는 메서드 구현
    - parseResponse
    - validateResponse

--- src/main/java/store/util/Parser.java
+++ src/main/java/store/util/Parser.java
@@ -10,14 +10,26 @@ public class Parser {
     private final static String DELIMITER = ",";
 
     public static List<String> parseItems(String input) {
-        validate(input);
+        validatePurchaseInput(input);
 
         return Stream.of(input.replaceAll("[\\[\\]]", "").split(DELIMITER))
                 .filter(item -> item.matches("[^\\-]+-\\d+"))
                 .collect(Collectors.toList());
     }
 
-    private static void validate(String input) {
+    public static boolean parseResponse(String input) {
+        validateResponse(input);
+
+        return input.equals("Y");
+    }
+
+    private static void validateResponse(String input) {
+        if (!input.equals("Y") && !input.equals("N")) {
+            throw new StoreException(ErrorMessage.INVALID_RESPONSE.getMessage());
+        }
+    }
+
+    private static void validatePurchaseInput(String input) {
         if (isNotMatch(input)) throw new StoreException(ErrorMessage.INVALID_FORMAT.getMessage());
     }
 
