db01ba29e7b39e695d9b80767967065db323da3d
StoneCAU <seokwoo1999@naver.com>
2024-11-11 09:38:17 +0900

    test(PromotionTest): 프로모션 객체 생성 테스트

---
+++ src/test/java/store/domain/PromotionTest.java
@@ -0,0 +1,33 @@
+package store.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.LocalDate;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import store.domain.promotion.Promotion;
+import store.domain.promotion.PromotionFactory;
+
+public class PromotionTest {
+
+    @Test
+    void 프로모션_객체_생성_테스트() {
+        // given
+        final PromotionFactory promotionFactory = new PromotionFactory();
+
+        // when
+        List<Promotion> actual = promotionFactory.initPromotions();
+        List<Promotion> expected = List.of(
+                new Promotion("탄산2+1", 2, 1,
+                LocalDate.of(2024,1,1), LocalDate.of(2024,12,31)),
+
+                new Promotion("MD추천상품", 1, 1,
+                        LocalDate.of(2024,1,1), LocalDate.of(2024,12,31))
+        );
+
+        // then
+        assertThat(actual)
+                .usingRecursiveFieldByFieldElementComparator()
+                .containsExactlyInAnyOrderElementsOf(expected);
+    }
+}
